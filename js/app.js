5eede8443cd765829a214b427d511feed1ff16da
Mukesh9784 <75660823+Mukesh9784@users.noreply.github.com>
2021-10-11 23:36:14 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,98 +1,3 @@
-// console.log('welcome to app.js');
-// showNotes();
-// let addBtn = document.getElementById("addBtn");
-// addBtn.addEventListener("click", function(e) {
-//     let addTxt = document.getElementById("addTxt");
-//     let notes = localStorage.getItem("notes");
-//     if (notes == null) {
-//         notesObj = [];
-//     }
-//     else {
-//         notesObj = JSON.parse(notes);
-
-//     }
-//     notesObj.push(addTxt.value);
-//     localStorage.setItem("notes", JSON.stringify(notesObj));
-//     addTxt.value = "";
-//     console.log(notesObj);
-// showNotes();
-
-// }) 
-// function showNotes()
-// {
-//     let notes = localStorage.getItem("notes");
-//     if(notes==null)
-//     {
-//         notesObj = [];
-//     }
-//     else{
-//         notesObj = JSON.parse(notes);
-//     }
-//     let html = "";
-//     notesObj.forEach(function(element,index){
-//         html += `  <div class="noteCard my-2 mx-2" style="width: 18rem;">
-//         <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
-//         <div class="card-body">
-//             <h5 class="card-title">Note ${index+1}</h5>
-//             <p class="card-text"> ${element}
-//             </p>
-//             <button href="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-//         </div>
-//     </div>`;
-
-
-//     });
-
-//     let notesElm  = document.getElementById('notes');
-//     if(notesObj.length!=0)
-//     {
-//         notesElm.innerHTML = html;
-//     }
-//     else{
-//         notesElm.innerHTML = `Nothing to Show! "Add a Note" section above to add notes`;
-
-//     }
-// }
-// //function to delete a note
-// function deleteNote(index)
-// {
-//    console.log("i am deleting" , index);  
-//    let notes = localStorage.getItem("notes");
-//    if(notes==null)
-//    {
-//        notesObj = [];
-//    }
-//    else{
-//        notesObj = JSON.parse(notes);
-//    }
-//    notesObj.splice(index,1);
-//    localStorage.setItem("notes", JSON.stringify(notesObj));
-//    showNotes();
-
-// }    
-// let search = document.getElementById('searchTxt');
-// searchTxt.addEventListener("input",function(){
-//     let inputVal = search.value;
-//     console.log("input event fired",inputVal);
-//     let noteCards = document.getElementsByClassName('noteCard');
-//     Array.from(noteCards).forEach(function(element)
-//     {
-
-//         let cardTxt = element.getElementByTagName("p")[0].innerText;
-//         console.log(cardTxt);
-//         if(cardTxt.includes(inputVal)){
-//             element.style.display = "block";
-//         }
-//         else
-//         {
-//             element.style.display = "none"; 
-//         }
-
-        
-
-//     })
- 
-// })
  
 console.log("Welcome to notes app. This is app.js");
 showNotes();
@@ -101,7 +6,7 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
-  let addtitle = document.getElementById("addTxt");
+  //let addtitle = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
@@ -109,11 +14,11 @@ addBtn.addEventListener("click", function(e) {
     notesObj = JSON.parse(notes);
   }
   notesObj.push(addTxt.value);
-  notesObj.push(addtitle.value);
+  //notesObj.push(addtitle.value);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   // localStorage.setItem("notes", JSON.stringify());
   addTxt.value = "";
-  addtitle.value = "";
+ // addtitle.value = "";
 //   console.log(notesObj);
   showNotes();
 });
